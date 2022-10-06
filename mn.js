006e3fdfcb23ad334c4962e9a03497c2f5a2a0ca
Aditya Shantilal Bathani <95112738+Aditya4478@users.noreply.github.com>
2022-10-06 19:54:02 +0530

    version2commit

--- mn.js
+++ mn.js
@@ -1,10 +1,9 @@
-//JAVASCRIPT for logics
-console.log('Welcome to Ex:2 - Magic Notes')
+// keep calling function shownotes to always disappear notes saved in local storage
 showNotes();
 
+//adding notes
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', function (e) {
-
     let addTxt = document.getElementById('addTxt');
     let notes = localStorage.getItem('notes');
     if (notes == null) {
@@ -13,45 +12,60 @@ addBtn.addEventListener('click', function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
+    //showing alert if notes is empty & updating falsy statement inside the object.
+    if(addTxt.value==''){
+        alert('😠😒😤Do not press ADD button without writting any note. !!')
+        addTxt.value = 'Empty Note 😥';
+    }
     notesObj.push(addTxt.value);
     localStorage.setItem('notes', JSON.stringify(notesObj));
     addTxt.value = "";
-    console.log(notesObj);
     showNotes();
 });
 
+// for showing notes
 function showNotes() {
     let notes = localStorage.getItem('notes');
+    let html = "";
     if (notes == null) {
+        // creating first time
         notesObj = [];
     }
     else {
+        // a ranom creation of text inside object
         notesObj = JSON.parse(notes);
     }
-    let html = "";
+    // updating in table
     notesObj.forEach(function (element, index) {
         html += `
-        <div class="noteCard card my-2 mx-2" style="width: 18rem">
-                <div class="card-body">
-                    <h5 class="card-title ct">Note ${index + 1}</h5>
-                    <p class="card-text cd">${element}</p>
-                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Remove
-                        &#128465;</button>
-                </div>
-            </div>`;
+            <tr>
+              <td align="center" style="font-size: 120%"" scope="row"><b>${index + 1}</b></td>
+              <td class="noteCard" style="font-family: Brush Script MT; font-size: 140%; letter-spacing: 1px;">
+              ${element}
+              </td>
+              <td align="center">
+                <button
+                  class="btn btn-sm btn-primary"
+                  style="background-color: rgb(204, 35, 35)"
+                  id="${index}" onclick="deleteNote(this.id)"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>`;
     });
-
-    let notesElm = document.getElementById('notes');
+    // if there are no notes available in object, then show message inside table
+    let notesElm = document.getElementById('notes1');
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
     }
     else {
-        notesElm.innerHTML = `You have not added a single note &#128551;&#128556;&#128577;. Add them QUICKLY !!!`;
+        notesElm.innerHTML = `<tr><td colspan="3"><h6 style="text-align: center;">You have not added a single note &#128551;&#128556;&#128577;. Add them QUICKLY !!!</h6></td></tr>`;
     }
 }
 
+//Deleting node from local storage
 function deleteNote(index) {
-    console.log('I am deleting', index);
     let notes = localStorage.getItem('notes');
     if (notes == null) {
         notesObj = [];
@@ -59,30 +73,49 @@ function deleteNote(index) {
     else {
         notesObj = JSON.parse(notes);
     }
-
+    // deleting a part of whole object.
     notesObj.splice(index, 1);
     localStorage.setItem('notes', JSON.stringify(notesObj));
     showNotes();
 }
 
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function () {
-
-    let inputVal = search.value.toLowerCase();
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function (element) {
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if (cardTxt.includes(inputVal)) {
-            element.style.display = "block";
+//searching function. First text input is converted to uppercase. then the content of table is converted to uppercase and compared.
+function searchFun() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        // searching in 2nd column of table (total cloumn : 0,1,2 => 1st column)
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            //displaying matched values
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
         }
-        else {
-            element.style.display = "none";
-        }
-    })
-})
+      }
+    }
+}
 
+//defined a function for playing audio
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
+// exporting content of object created.
 var data = [];
 function save_func() {
+    //activating animation to disapear the table.
+    document.getElementById('myTable').style.cssText = `
+          animation-name: myAnimation;
+          animation-duration: 2000ms;
+          animation-fill-mode: forwards;
+    `
     let notes = localStorage.getItem('notes');
     if (notes == null) {
         notesObj = [];
@@ -90,14 +123,22 @@ function save_func() {
     else {
         notesObj = JSON.parse(notes);
     }
+    //creating a message to be print along with the content fetched from local storage
     notesObj.forEach(function (element, index) {
-        data.push("Content of Note " + index +" is -->> "+ element);
+        data.push("Content of Note " + (index+1) +" is -->> "+ element);
     });
     var data_string = JSON.stringify(data);
     var file = new Blob([data_string], { type: "text" });
     var anchor = document.createElement("a");
+    //file format
     anchor.href = URL.createObjectURL(file);
+    // file name
     anchor.download = "Magical-Notes.txt";
     anchor.click();
-    alert("Notes Saved Succesfully")
+    //calling sound function
+    play();
+    //clearing stored notes.
+    localStorage.clear();
+
+    alert("Notes Saved Successfully")
 }
